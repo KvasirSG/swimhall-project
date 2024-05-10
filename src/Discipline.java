3b42c731fbb00bac7637f8e12deaa11bd13c2643
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-05-10 12:32:59 +0100

    Added Db support

--- src/Discipline.java
+++ src/Discipline.java
@@ -23,9 +23,10 @@ public class Discipline
         return name;
     }
 
-    public void updateRecord(Swimmer swimmer, double newRecord, LocalDate recordDate)
+    public void updateRecord(Swimmer swimmer, double newRecord, LocalDate recordDate, DatabaseManager dBManager)
     {
         Record record = new Record(swimmer.getMemberID(),this.disciplineID,newRecord,recordDate);
+        record.registerRecord(dBManager);
     }
 
     public void registerDiscipline(DatabaseManager dBManager){
