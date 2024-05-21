9e9b980d4d6b6a372e11454cfaea36dfb84ebedb
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-05-21 12:23:47 +0100

    added getMemberFromMembershipType

---
+++ src/swimapp/backend/GuiInterface.java
@@ -0,0 +1,20 @@
+package swimapp.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuiInterface {
+    public static List<Member> getMemberFromMembershipType(int membershipTypeID){
+        DatabaseManager db = new DatabaseManager();
+        List<Member> dbAllMembers= db.getAllMembers();
+        db.closeConnection();
+        List<Member> dbAllMembersType= new ArrayList<Member>();
+
+        for(Member member : dbAllMembers){
+            if(member.getMembershipTypeID() == membershipTypeID){
+                dbAllMembersType.add(member);
+            }
+        }
+        return dbAllMembersType;
+    }
+}
