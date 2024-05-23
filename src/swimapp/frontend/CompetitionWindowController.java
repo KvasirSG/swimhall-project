71e9e0d0e90bbfcfa3471044c21a0bcddbdf7ebd
Nikolaj Pirum <nikolajpirum.10@gmail.com>
2024-05-23 10:43:37 +0200

    Comp gui

--- src/swimapp/frontend/CompetitionWindowController.java
+++ src/swimapp/frontend/CompetitionWindowController.java
@@ -1,4 +1,44 @@
 package swimapp.frontend;
 
-public class CompetitionWindowController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import swimapp.backend.DatabaseManager;
+import swimapp.backend.Record;
+import swimapp.frontend.Main;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CompetitionWindowController
+{
+    @FXML
+    private Button btn_CBack;
+
+    @FXML
+    private TextField tf_CCID;
+
+    @FXML
+    private TextField tf_CSID;
+
+    @FXML
+    private TextField tf_CDISC;
+
+    @FXML
+    private DatePicker tf_CNRT;
+
+    @FXML
+    private TextField tf_CLOC;
+
+    @FXML
+    private Button btn_CDesM;
+
+    private DatabaseManager dbManager;
+
+
+    
 }
